0e0d58f5a3e979b4fef586a5c55fc352bd6c0b25
PollyGotACracker <bitterns96@gmail.com>
2026-01-13 04:06:04 +0900

    feat: Apply custom hook to todo add

--- app/_components/TodoAdd.tsx
+++ app/_components/TodoAdd.tsx
@@ -1,27 +1,37 @@
 'use client';
 
-import { ChangeEvent, useState } from "react";
-import { useFormStatus } from "react-dom";
+import { ChangeEvent, useEffect, useState } from "react";
 import { HOME_TEXT } from "@/constants/messages";
 import PlusSvg from '@/icons/plus.svg';
 import Input from "@/components/Input";
 import Button from "@/components/Button";
 import { createItem } from '../_actions';
+import useServerAction from "@/hooks/useServerAction";
 
 export default function TodoAdd() {
-    const [name, setName] = useState<string>("");
-    const { pending } = useFormStatus();
-    const isFilled = name.trim().length > 0;
+    const [name, setName] = useState("");
+    const [isFilled, setIsFilled] = useState(false);
+    const [createAction, isCreatePending, isCreateSuccess] = useServerAction(createItem);
+
+    useEffect(() => {
+        // name 보다 success 가 우선순위 높음
+        if (name.trim().length > 0) {
+            setIsFilled(true)
+        }
+        if (isCreateSuccess) {
+            setIsFilled(false)
+        }
+    }, [name, isCreateSuccess])
 
     const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
         setName(e.target.value);
     };
 
     return (
-        <form action={createItem} className="w-full flex justify-center gap-x-[8px]">
+        <form action={createAction} className="w-full flex justify-center gap-x-[8px]">
             <Input id="name" name="name" placeholder={HOME_TEXT.CREATE_PLACEHOLDER} onChange={onChangeInput} />
             <Button
-                disabled={!isFilled || pending}
+                disabled={!isFilled || isCreatePending}
                 type="submit"
                 Icon={PlusSvg}
                 text={HOME_TEXT.CREATE_BUTTON}
