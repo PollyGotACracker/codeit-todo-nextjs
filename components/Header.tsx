4bf01a9275b3e567858b76b053bf3f81a7f942f5
PollyGotACracker <bitterns96@gmail.com>
2026-01-14 07:45:24 +0900

    feat: Add reloading to logo button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,25 @@
-import Link from "next/link";
+'use client';
+
+import { useRouter, usePathname } from "next/navigation";
 import { ROUTE } from "@/constants/route";
+
 export default function Header() {
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const handleNavigation = () => {
+        if (pathname === ROUTE.HOME) {
+            window.location.reload();
+        } else {
+            router.push(ROUTE.HOME);
+        }
+    }
+
     return (
         <header className="w-full h-[60px] pl-[16px] flex items-center bg-white border-b border-b-slate-200">
-            <Link href={ROUTE.HOME}>
+            <button type="button" title="Do It" onClick={handleNavigation}>
                 <div className="logo" />
-            </Link>
+            </button>
         </header>
     )
 }
